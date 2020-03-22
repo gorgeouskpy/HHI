1b95b701eb1578d1514a1882e8b4dd6ac0b77967
gorgeouskpy <1284836331@qq.com>
2020-03-22 12:09:09 +0800

    second commit

---
+++ html/script/bottom.js
@@ -0,0 +1,27 @@
+const bottomarea = {
+    template: `
+    <div class="container-fluid">
+      <div class="row bottomarea">
+        <div class="col-4">
+          <h5 class="bottomlist-title">团队</h5>
+          <ul class="bottomlist">
+            <li>Member 1</li>
+            <li>Member 2</li>
+            <li>Member 3</li>
+            <li>Member 4</li>
+            <li>Member 5</li>
+            <li>Member 6</li>
+          </ul>
+        </div>
+        <div class="col-4">
+          <h5 class="bottomlist-title">作品</h5>
+          <ul class="bottomlist">
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    `,
+  };
