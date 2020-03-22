c093cfc78909c292b6a519c27737fc629515db6e
gorgeouskpy <1284836331@qq.com>
2020-03-22 10:39:17 +0800

    breach 2

---
+++ 小强学英语/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import HelloWorldApp from './component/main/main';
+
+export default class App extends Component {
+  render() {
+    return (
+      <HelloWorldApp></HelloWorldApp>
+    );
+  }
+}
+
---
+++ 小强学英语/component/main/main.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Text, View, Image, StyleSheet, } from 'react-native';
+
+/* 导入外部组件类 */
+import TabNavigator from 'react-native-tab-navigator';
+import Home from '../home/home';
+import Explore from '../explore/explore';
+import Profile from '../profile/profile';
+
+export default class HelloWorldApp extends Component {
+  //初始化函数状态机
+state = {
+    selectedTab: 'home',
+}
+
+render() {
+return (
+    <TabNavigator>
+        {/*首页*/}
+        <TabNavigator.Item
+            title="Home"
+            renderIcon={() => <Image style={styles.iconStyle} source = {{uri: 'https://gorgeousbed.oss-cn-beijing.aliyuncs.com/static/logo.ico'}}></Image>}
+            renderSelectedIcon={()=> <Image style={styles.iconStyle} source = {{uri: 'https://gorgeousbed.oss-cn-beijing.aliyuncs.com/static/hf.jpg'}}></Image>}
+            onPress={()=>{this.setState({selectedTab: 'home'})}}
+            selected={this.state.selectedTab === 'home'}
+            selectedTitleStyle={styles.titleStyle}
+            >
+                <Home></Home>
+        </TabNavigator.Item>
+        {/*探索*/}
+        <TabNavigator.Item
+            title="Explore"
+            renderIcon={() => <Image style={styles.iconStyle} source = {{uri: 'https://gorgeousbed.oss-cn-beijing.aliyuncs.com/static/logo.ico'}}></Image>}
+            renderSelectedIcon={()=> <Image style={styles.iconStyle} source = {{uri: 'https://gorgeousbed.oss-cn-beijing.aliyuncs.com/static/hf.jpg'}}></Image>}
+            onPress={()=>{this.setState({selectedTab: 'exp'})}}
+            selected={this.state.selectedTab === 'exp'}
+            selectedTitleStyle={styles.titleStyle}
+            >
+                <Explore></Explore>
+        </TabNavigator.Item>
+        {/*档案*/}
+        <TabNavigator.Item
+            title="Profile"
+            renderIcon={() => <Image style={styles.iconStyle} source = {{uri: 'https://gorgeousbed.oss-cn-beijing.aliyuncs.com/static/logo.ico'}}></Image>}
+            renderSelectedIcon={()=> <Image style={styles.iconStyle} source = {{uri: 'https://gorgeousbed.oss-cn-beijing.aliyuncs.com/static/hf.jpg'}}></Image>}
+            onPress={()=>{this.setState({selectedTab: 'pro'})}}
+            selected={this.state.selectedTab === 'pro'}
+            selectedTitleStyle={styles.titleStyle}
+        >
+                <Profile></Profile>
+        </TabNavigator.Item>
+    </TabNavigator>
+);
+}
+}
+
+const styles = StyleSheet.create({
+iconStyle: {
+    height: 30,
+    width: 30,
+},
+titleStyle: {
+    color: 'red',
+}
+})
